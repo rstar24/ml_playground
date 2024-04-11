3994ce199cf7b90e44da4f990cadee949011abfa
rstar24 <rishabhrathod20@gmail.com>
2024-04-11 13:48:20 +0530

    Removed Unnecessary Files

--- test_ollama.py
+++
@@ -1,9 +0,0 @@
-response = ollama.chat(model = 'llama32',messages=[
-    {
-        'role': 'user',
-        'content': 'why is the sky blue',
-
-    },
-])
-
-print(response['messages']['content'])
